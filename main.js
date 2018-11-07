b6d7405332a93c6c931ff9358fbfc5c4a8e19041
Ebenezer Ajaero <ebenezerajaero@macbook-air.home>
2018-11-07 21:15:25 +1030

    sierspinski done

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-var c = document.getElementById("myCanvas")
-var ctx = c.getContext("2d")
-
+var currentAnimation;
+function resizeCanvas(c) {
+    c.style.width ='100%';
+    c.style.height='100%';   
+    c.width  = c.offsetWidth;
+    c.height = c.offsetHeight;
+}
 // sierspinksi process specification
 // Final recursion should result in a triangle drawn and filled at positions (a, b, c) - [(ax, ay), (bx, by), (cx, cy)]
 // if final iteration then draw otherwise:
@@ -14,7 +18,7 @@ function genMid(a, b) {
   return (a + b) / 2
 }
 
-function sier(ax, ay, bx, by, cx, cy, n) {
+function sier(ax, ay, bx, by, cx, cy, n, ctx) {
   if (n > 0) {
     //gen mid points
     var abx = genMid(ax, bx)
@@ -24,15 +28,51 @@ function sier(ax, ay, bx, by, cx, cy, n) {
     var cax = genMid(cx, ax)
     var cay = genMid(cy, ay)
 
-    sier(abx, aby, bx, by, bcx, bcy, n-1)
-    sier(cax, cay, ax, ay, abx, aby, n-1)
-    sier(bcx, bcy, cx, cy, cax, cay)
+    sier(abx, aby, bx, by, bcx, bcy, n-1, ctx)
+    sier(cax, cay, ax, ay, abx, aby, n-1, ctx)
+    sier(bcx, bcy, cx, cy, cax, cay, n-1, ctx)
 
 
   } else {
+        ctx.beginPath()
+      ctx.fillStyle = randomColor()
+      ctx.strokeStyle = randomColor()       
+        console.log(randomColor())
+        console.log(randomColor())
+      ctx.moveTo(ax, ay)
+      ctx.lineTo(bx, by)
+      ctx.lineTo(cx, cy)
+      ctx.lineTo(ax, ay)
+              ctx.fill()
+        ctx.stroke()
 
-    //draw(ax, ay, bx, by, cx, cy)
   }
+}
+
+var sierInter = 0;
+var sideLength = 0;
+var height = 0;
+
+function drawSier() {
+     var c = document.getElementById("myCanvas")
+
+    var ctx = c.getContext("2d")
+    ctx.clearRect(0,0,c.width, c.height)
+        ctx.beginPath()
+
+        sier((c.width / 2) - (sideLength / 2),(c.height/2) + (height/2), c.width/2, (c.height / 2) - (height/2), (c.width/2) + (sideLength / 2), (c.height/2) + (height/2), sierInter, ctx)
+   
+
+        
+    if (sierInter < 7) {
+     sierInter++
+    }
+    else {
+        sierInter = 0
+
+    } 
+        console.log(sierInter)
+
 }
 
 
@@ -44,15 +84,22 @@ function mandlebrot() {
 function sierspinski() {
    $(".is-active").removeClass("is-active")
     $("#sier").addClass("is-active")
+    var c = document.getElementById("myCanvas")
 
-    ctx.fillStyle = "#FF0000"
-    ctx.strokeStyle = 'black'
-    ctx.lineWidth = 3
+    resizeCanvas(c)
+    var ctx = c.getContext("2d")
 
-    sier(100,100, 200, 200, 300, 300, 0)
+
+    console.log(c.width)  
+
+    sideLength = c.width / 2
+    height = Math.sqrt(Math.pow(sideLength, 2) - Math.pow(sideLength / 2, 2))
+
+    console.log(sideLength)
+    console.log(height)
     
-    ctx.fill()
-    ctx.stroke()
+    currentAnimation = window.setInterval(drawSier, 1000)
+
 }
 
 function koch() {
